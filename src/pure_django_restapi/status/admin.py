21cf5518ab1d24323dfe20144adc1df331db4fe4
Ritwick Abhishek <abhishek.ritwick@gmail.com>
2019-10-21 16:54:07 +0530

    Second Commit- Added app Status

---
+++ src/pure_django_restapi/status/admin.py
@@ -0,0 +1,13 @@
+from django.contrib import admin
+from status.models import Status
+from status.forms import StatusForm
+# Register your models here.
+
+class StatusAdmin(admin.ModelAdmin):
+    list_display = ['user', '__str__', 'image']
+    form = StatusForm
+    # class Meta:
+    #     model = Status
+
+
+admin.site.register(Status, StatusAdmin)
